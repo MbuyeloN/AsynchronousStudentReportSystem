0b49533c9b94c43321cca855c49979d57aa3c8fc
MbuyeloN <ST10473424@rcconnect.edu.za>
2026-08-14 22:58:59 +0200

    Add Student class for asynchronous report system.

---
+++ AsynchronousStudentReportSystem/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace AsynchronousStudentReportSystem
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public string StudentNumber { get; set; }
+        public double Marks { get; set; }
+
+        public Student(string studentName, string studentNumber, double marks)
+        {
+            StudentName = studentName;
+            StudentNumber = studentNumber;
+            Marks = marks;
+        }
+    }
+}
